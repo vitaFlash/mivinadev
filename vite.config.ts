34c2d71dadbc957e56c7816c8da418b31c712951
vitaFlash <106696593+vitaFlash@users.noreply.github.com>
2026-02-08 00:12:19 +0100

    Update Vite configuration for React and server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
     },
   },
-});
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
