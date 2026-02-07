83a9543ecdab29e3b95ebfd8903c545fd55d4871
vitaFlash <106696593+vitaFlash@users.noreply.github.com>
2026-02-07 23:49:47 +0100

    Refactor plugins array formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Set this to your repository name!
+  base: "/mivinadev/", 
+
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +15,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
